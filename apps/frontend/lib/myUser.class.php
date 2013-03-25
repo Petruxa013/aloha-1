f8a76f1020ccde97d1c2a08a1cbfce9d56681236
Alexander M <a.manichev@gmail.com>
2013-03-25 16:56:27 +0600

    configure project

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -7,4 +7,20 @@ class myUser extends sfGuardSecurityUser
 		parent::__construct($dispatcher, $storage);
 		$this->setCulture('ru_RU');
 	}
+
+	/**
+	 * Returns the sfGuardUser object's username.
+	 *
+	 * @return string
+	 */
+	public function getUsername()
+	{
+		$user = $this->getGuardUser();
+		if($user)
+			return $this->getGuardUser()->getUsername();
+
+		return null;
+	}
+
 }
+
---
+++ apps/frontend/modules/pageParts/templates/_headerMenu.php
@@ -0,0 +1,33 @@
+<div class="navbar-wrapper">
+		<div class="navbar">
+			<div class="navbar-inner">
+				<!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+				<button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar" type="button">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="<?php echo url_for('@homepage') ?>">Панель управления проектом</a>
+				<!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="dropdown">
+							<a data-toggle="dropdown" class="dropdown-toggle" href="#">Пользователи
+								<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><?php echo link_to('Пользователи', 'sf_guard_user') ?></li>
+								<li><?php echo link_to('Группы', 'sf_guard_group') ?></li>
+								<li><?php echo link_to('Разрешения', 'sf_guard_permission') ?></li>
+							</ul>
+						</li>
+						</ul>
+					<ul id="main-menu-right" class="nav pull-right">
+						<li><?php echo link_to('Выход', 'signout') ?></li>
+					</ul>
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+			<!-- /.navbar-inner -->
+		</div>
+		<!-- /.navbar -->
+</div>
