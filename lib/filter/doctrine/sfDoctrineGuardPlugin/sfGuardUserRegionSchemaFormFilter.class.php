5c1732df619433597a809222d4c6fec51c05f732
Alexander M <a.manichev@gmail.com>
2013-04-01 15:46:02 +0600

    add regions and cities to users

---
+++ lib/filter/doctrine/sfDoctrineGuardPlugin/sfGuardUserRegionSchemaFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardUserRegionSchema filter form.
+ *
+ * @package    aloha
+ * @subpackage filter
+ * @author     Alexander Manichev a.manichev@gmail.com
+ * @version    SVN: $Id: sfDoctrinePluginFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardUserRegionSchemaFormFilter extends PluginsfGuardUserRegionSchemaFormFilter
+{
+  public function configure()
+  {
+  }
+}
