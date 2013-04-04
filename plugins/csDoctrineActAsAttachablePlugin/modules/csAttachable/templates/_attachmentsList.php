e73b72255330463d09ac2315588adfa872f2797d
Alexander M <a.manichev@gmail.com>
2013-04-04 21:48:06 +0600

    stylish additional worksheet files page after approve

--- plugins/csDoctrineActAsAttachablePlugin/modules/csAttachable/templates/_attachmentsList.php
+++ plugins/csDoctrineActAsAttachablePlugin/modules/csAttachable/templates/_attachmentsList.php
@@ -1,15 +1,22 @@
 <?php if ($attachments = $object->getAttachments()): ?>
 	<?php if ($attachments->count()): ?>
-		<ul class='attachment'>
+		<?php $imageAttachments = ''; $audioAttachment = ''; ?>
 			<?php foreach ($attachments as $attachment): ?>
-				<li>
+
 					<?php if ($attachment->getType() == 'image'): ?>
-						<?php echo link_to(image_tag($attachment->getAttachmentResizedRoute(10, 10)), image_path($attachment->getAttachmentResizedRoute(50, 50)), 'class=link target=_blank') ?>
-					<?php else: ?>
-						<?php echo link_to($attachment->getTitle(), image_path($attachment->getAttachmentRoute()), 'class=link target=_blank') ?>
+						<?php $imageAttachments .= '<li>'.link_to(image_tag($attachment->getAttachmentResizedRoute(10, 10)), image_path($attachment->getAttachmentResizedRoute(50, 50)), 'class=link target=_blank').'</li>' ?>
+					<?php elseif ($attachment->getType() == 'audio'): ?>
+						<?php $audioAttachment .= '<li>'.link_to($attachment->getTitle(), image_path($attachment->getAttachmentRoute()), 'class=link target=_blank').'</li>' ?>
 					<?php endif;?>
-				</li>
+
 			<?php endforeach ?>
+		<h2><?php echo translateAttachmentType(trim('image')) ?></h2>
+		<ul class='attachment'>
+			<?php echo $imageAttachments ?>
+		</ul>
+		<h2><?php echo translateAttachmentType(trim('audio')) ?></h2>
+		<ul class='attachment'>
+			<?php echo $audioAttachment ?>
 		</ul>
 	<?php endif ?>
 <?php endif ?>
