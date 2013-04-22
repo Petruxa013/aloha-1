a3387948cd6d2228c5451e3d1e5422d3eb904223
Alexander M <a.manichev@gmail.com>
2013-04-22 20:49:40 +0600

    client and auditor panels pagination fix

--- apps/frontend/modules/auditor_panel/templates/_paginator.php
+++ apps/frontend/modules/auditor_panel/templates/_paginator.php
@@ -1,19 +1,21 @@
 <?php if ($pager->haveToPaginate()): ?>
 <div class="pagination pagination-centered">
 <ul>
-	<?php if (!$pager->isFirstPage()): ?>
 	<?php if(mb_strlen($pager->getFilters()) > 5): ?>
-        <li><a href="<?php echo url_for('@auditor_panel_filter')?>?page=<?php echo $pager->getPreviousPage() ?><?php echo $pager->getFilters() ?>">&laquo;</a></li>
+        <li><a href="<?php echo url_for('@auditor_panel_filter')?>?page=1<?php echo $pager->getFilters() ?>">&laquo;</a></li>
 	<?php else: ?>
-		<li><a href="<?php echo url_for('@auditor_panel')?>?page=<?php echo $pager->getPreviousPage() ?>">&laquo;</a></li>
+		<li><a href="<?php echo url_for('@auditor_panel')?>?page=1">&laquo;</a></li>
 	<?php endif; ?>
-	<?php else: ?>
-		<li class="active"><a href="#">&laquo;</a></li>
+
+	<?php if (!$pager->isFirstPage()): ?>
+		<?php if(mb_strlen($pager->getFilters()) > 5): ?>
+	        <li><a href="<?php echo url_for('@auditor_panel_filter')?>?page=<?php echo $pager->getPreviousPage() ?><?php echo $pager->getFilters() ?>">&larr;</a></li>
+		<?php else: ?>
+			<li><a href="<?php echo url_for('@auditor_panel')?>?page=<?php echo $pager->getPreviousPage() ?>">&larr;</a></li>
+		<?php endif; ?>
 	<?php endif; ?>
+
 	<?php foreach ($pager->getLinks() as $page): ?>
-		<?php if($page == $pager->getFirstPage())
-		         continue;
-		?>
 	<?php if ($page == $pager->getPage()): ?>
         <li class="active"><a href="#"><?php echo $page ?></a></li>
 		<?php else: ?>
@@ -28,6 +30,22 @@
 		<?php endif; ?>
 		<?php endif; ?>
 	<?php endforeach; ?>
+
+	<?php if (!$pager->isLastPage()): ?>
+		<?php if(mb_strlen($pager->getFilters()) > 5): ?>
+	        <li><a href="<?php echo url_for('@auditor_panel_filter')?>?page=<?php echo $pager->getNextPage() ?><?php echo $pager->getFilters() ?>">&rarr;</a></li>
+		<?php else: ?>
+			<li><a href="<?php echo url_for('@auditor_panel')?>?page=<?php echo $pager->getNextPage() ?>">&rarr;</a></li>
+		<?php endif; ?>
+	<?php endif; ?>
+
+
+	<?php if(mb_strlen($pager->getFilters()) > 5): ?>
+        <li><a href="<?php echo url_for('@auditor_panel_filter')?>?page=<?php echo $pager->getLastPage() ?><?php echo $pager->getFilters() ?>">&raquo;</a></li>
+	<?php else: ?>
+		<li><a href="<?php echo url_for('@auditor_panel')?>?page=<?php echo $pager->getLastPage() ?>">&raquo;</a></li>
+	<?php endif; ?>
+
 </div>
 </ul>
 <?php endif; ?>
