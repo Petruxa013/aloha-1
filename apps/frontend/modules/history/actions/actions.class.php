d8206d5c090e3b6abae653884438bc2dec0e6378
Alexander M <a.manichev@gmail.com>
2013-04-24 14:44:30 +0600

    add history module

---
+++ apps/frontend/modules/history/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/historyGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/historyGeneratorHelper.class.php';
+
+/**
+ * history actions.
+ *
+ * @package    aloha
+ * @subpackage history
+ * @author     Alexander Manichev a.manichev@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class historyActions extends autoHistoryActions
+{
+}
---
+++ apps/frontend/modules/history/lib/historyGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * history module helper.
+ *
+ * @package    aloha
+ * @subpackage history
+ * @author     Alexander Manichev a.manichev@gmail.com
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class historyGeneratorHelper extends BaseHistoryGeneratorHelper
+{
+}
